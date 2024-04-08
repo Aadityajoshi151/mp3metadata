63cd1b06efce692543e7a25072ebec43644279ed
Aadityajoshi151 <aadityajoshi151@gmail.com>
2024-04-08 11:40:54 +0530

    adds script

--- src/mp3metadata.py
+++ src/mp3metadata.py
@@ -1,7 +1,20 @@
-#imports
+from mutagen.easyid3 import EasyID3
+import os
 
 def main():
-    print("Main Begins")
+    songs_folder = "Songs/"
+    for song in os.listdir(songs_folder):
+        print(song)
+        if ".mp3" in song:
+            audio = EasyID3(f"{songs_folder}{song}")
+            audio['title'] = song.replace(".mp3", "")
+            audio.save()
+            print("Metadata updated!")
+            print("="*50)
+
+        else:
+            print("Skipped!")
+        
 
 if __name__ == "__main__":
     main()
